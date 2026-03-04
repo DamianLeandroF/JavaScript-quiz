1af863303191e36e86d70488af2270f943bd1f7d
DamianLeandroF <damian.fregocini@gmail.com>
2026-03-03 22:39:38 -0300

    borrar log

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -19,8 +19,7 @@ export const useQuestionsStore = create <State>()(persist((set, get) =>{
         currentQuestion : 0,
         fetchQuestions : async (limit: number)=>{
             const res = await fetch('/data.json')
-            const json = await res.json()
-            console.log(json) 
+            const json = await res.json() 
             const questions = json.sort(()=> Math.random() -0.5).slice(0,limit) // esto sirve para desordenar los elementos que devuelve el fetch
             set({questions})
         },
